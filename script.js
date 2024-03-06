0e98c0298a7b813c5d607a13d3ed487ec7af695f
Elias. F <elias.fransson086@gmail.com>
2024-03-06 23:31:13 +0100

    Added RSS feed

--- script.js
+++ script.js
@@ -40,3 +40,26 @@ document.addEventListener("DOMContentLoaded", function() {
       video.volume = parseFloat(this.value);
     });
 });
+
+function fetchRSSFeed(url, count) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            const parser = new DOMParser();
+            const xmlDoc = parser.parseFromString(data, "text/xml");
+            const items = xmlDoc.getElementsByTagName("item");
+
+            for (let i = 0; i < count && i < items.length; i++) {
+                const title = items[i].getElementsByTagName("title")[0].textContent;
+                const link = items[i].getElementsByTagName("link")[0].textContent;
+                const description = items[i].getElementsByTagName("description")[0].textContent;
+
+                const itemElement = document.createElement("div");
+                itemElement.innerHTML = `<h3><a href="${link}">${title}</a></h3><p>${description}</p>`;
+                document.getElementById("rss-feed").appendChild(itemElement);
+            }
+        })
+        .catch(error => console.error("Error fetching RSS feed:", error));
+}
+
+fetchRSSFeed("https://raw.githubusercontent.com/IceQ1337/CS-RSS-Feed/master/feeds/updates-feed-en.xml", 3);
